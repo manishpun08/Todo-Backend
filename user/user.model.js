63b876b7acc68a5d7d449380df549c85bdf8ca86
manishpun08 <manishpunmagar6@gmail.com>
2024-06-21 11:46:15 +0545

    register and login

--- user/user.model.js
+++ user/user.model.js
@@ -1,29 +1,38 @@
 import mongoose from "mongoose";
 
 // set schema
-const userSchema = mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-    trim: true,
-    maxlength: 55,
+const userSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+      maxlength: 55,
+    },
+    email: {
+      type: String,
+      required: true,
+      trim: true,
+      maxlength: 55,
+      lowercase: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      trim: true,
+      minlength: 4,
+    },
+    gender: {
+      type: String,
+      enum: ["male", "female", "preferNotToSay"],
+      required: false,
+    },
   },
-  email: {
-    type: String,
-    required: true,
-    trim: true,
-    maxlength: 55,
-    lowercase: true,
-    unique: true,
-  },
-  password: {
-    type: String,
-    required: true,
-    trim: true,
-    maxlength: 15,
-    unique: true,
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
 // create table
 export const User = mongoose.model("User", userSchema);
